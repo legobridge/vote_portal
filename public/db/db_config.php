b44691f3a1f298f7deb51768e6febc037c66fdaa
Kushal-sophos <f2015289@hyderabad.bits-pilani.ac.in>
2017-03-14 23:08:13 +0530

    Database Population

---
+++ public/db/db_config.php
@@ -0,0 +1,9 @@
+<?php
+
+    // display errors, warnings, and notices
+    ini_set("display_errors", true);
+    error_reporting(E_ALL);
+
+    $mysqli = new mysqli("localhost", "nickfury95", "v1kCjsvLYytrBTGV", "elections");
+    
+?>
---
+++ public/db/populate.php
@@ -0,0 +1,27 @@
+<?php
+    require("db_config.php");
+    if (empty($_GET["file_name"]))
+    {
+        echo "Please Specify a File Name";
+    }
+    else
+    {
+        $file_name = $_GET["file_name"];
+        if (($file = @fopen($file_name, 'r')) !== FALSE)
+        {
+            while(!feof($file))
+            {
+                $row = fgetcsv($file);
+                $id_no = hash("sha256", $row[0]);
+                $pass = hash("sha256", $row[1]);
+                $bhavan = $row[2];
+                $qu = "INSERT INTO `voters` (`id_no`, `pass`, `bhavan`) VALUES ('". $id_no . "', '" . $pass . "', '" . $bhavan . "')";
+                $mysqli -> query($qu);
+            }
+        }
+        else
+        {
+            echo "Please Specify a Valid File Name";
+        }
+    }    
+?>
